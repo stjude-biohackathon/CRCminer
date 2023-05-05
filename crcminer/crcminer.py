998c04904208c604b2ac30064bf4923a0c04aa13
Jared Andrews <jared.andrews07@gmail.com>
2023-05-05 15:35:01 -0500

    start updating click commands

--- crcminer/crcminer.py
+++ crcminer/crcminer.py
@@ -6,21 +6,21 @@ def cli_tools():
     pass
 
 @cli_tools.command(name='mine', help='mines for CRC')
-@click.option('--fasta', type=click.Path(),help='fasta')
-@click.option('--enhancer', type=click.Path(), help='ROSE2 output of annotated (super)enhancers')
-@click.option('--mapping', type=click.Path(), help='Motif ID to gene ID mapping file.')
-def test(fasta,enchancer,mapping):
+@click.option('--fasta', type=click.Path(),help='Genome FASTA file.')
+@click.option('--enhancer', type=click.Path(), help='ROSE2 output of annotated (super)enhancers.')
+@click.option('--subpeaks', type=click.Path(), help='BED file of regions to use for motif scanning, e.g. ATAC peaks. Will be limited to those that overlap with enhancers.')
+@click.option('--mapping', type=click.Path(), help='Motif accession to gene ID mapping file.')
+def test(fasta,enhancer,mapping):
     pass
 
-@cli_tools.command(name='compare', help='compare two networks')
+@cli_tools.command(name='compare', help='Compare output from two CRCminer mine runs')
 @click.option('--edgelist1', type=click.Path(), help='edgelist from sampleX')
 @click.option('--edgelist2', type=click.Path(), help='edgelist from sampleY')
 def test2(edgelist1,edgelist2):
     pass
 
 @cli_tools.command(name='report', help='report vizualization')
-@click.option('--indir',type=click.Path(),help='input directory CRCminer results')
-
+@click.option('--indir',type=click.Path(),help='Input directory containing CRCminer results.')
 def test3(indir):
     pass
 
